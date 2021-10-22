2ecf8181cd508d70eb14d9bac7dda40cd6dec8d2
Nikolai Kozak <nikokozak@gmail.com>
2021-10-21 20:56:00 -0600

    add tree functionality for sass

--- commander/sass.rb
+++ commander/sass.rb
@@ -14,7 +14,7 @@ class Sass
   end
 
   def self.is_sass?(file)
-    /\.s(a|c)ss\z/.match(file)
+    /\.s(a|c)ss\z/.match(file) ? true : false
   end
 
   def self.call_sass(input_file, output_file=nil)
@@ -39,9 +39,28 @@ class Sass
         Dir.children(".").each { |f| Sass.run(File.join(file_path, f)) } 
       end
     end
+  end
 
+  def self.tree(file_path='.')
+    file_path = Pathname.new(file_path)
+
+    result = {}
+
+    Dir.chdir(file_path) do
+      Dir['**/*'].each do |f|
+        if File.file?(f) && is_sass?(f)
+          path = Pathname.new(f)
+          category = path.dirname.basename.to_s == "." ? file_path : path.dirname.basename
+          filename = Sass.filename(f)
+          name = filename.gsub(/[\-_]/, " ").capitalize
+          style = File.read(File.join(path.dirname, filename + ".css"))
+          tag = filename + ".svg"
+
+          result[category.to_s] = (result[category.to_s] || []).push({name: name, style: style, tag: tag})
+        end
+      end
+    end
+    
+    result
   end
 end
-
-Sass.new('test').run
-
