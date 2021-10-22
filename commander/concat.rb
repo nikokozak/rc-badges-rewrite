5e4c3a8bacd2968323d7d8663f6adf77e832d73f
Nikolai Kozak <nikokozak@gmail.com>
2021-10-21 21:48:38 -0600

    safely write html

--- commander/concat.rb
+++ commander/concat.rb
@@ -1,14 +1,23 @@
 require_relative 'sass.rb'
 require 'erb'
+require 'fileutils'
 
 tree = Sass.new('test').tree
-p tree
 
 template = ERB.new(File.read("commander/template.erb"))
-#result = template.result_with_hash(categories: tree)
+result = template.result_with_hash(categories: tree)
 
-#File.write("template_out.html", result)
+def write_safe(file, content)
+  FileUtils.mkdir_p('.safe') if not File.exist?('.safe')
 
+  if File.exist? file
+    FileUtils.cp(file, ".safe/", preserve: false)
+  end
+
+  File.write(file, content)
+end
+
+write_safe("index.html", result)
 
 
 
