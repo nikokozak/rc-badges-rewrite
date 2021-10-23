3723fcd007e6a7ead0e9e06226c1fed4328b7066
Nikolai Kozak <nikokozak@gmail.com>
2021-10-23 17:20:18 -0600

    renderer documentation and tests

---
+++ commander/renderer.rb
@@ -0,0 +1,67 @@
+require 'fileutils'
+require 'erb'
+
+class Renderer
+  attr_accessor :queue
+
+  ##
+  # Creates a class instance with an internal queue of .erb files to process.
+  # These files do not include "_*.erb" files, which are considered mixins
+  # for parent files only. Can be instantiated with +[]+ as well.
+  # Params:
+  # +directory+:: directory to search for .erb files.
+
+  def initialize(directory=".")
+    @directory = directory
+    @queue = Dir[File.join("**", '[^_]*.erb'), base: directory]
+  end
+
+  def self.[](directory=".")
+    Renderer.new(directory)
+  end
+
+  ##
+  # Renders html files from .erb files, rendering mixins as well. Can pass a
+  # an optional +out+ directory for the files to be saved to, otherwise they are
+  # rendered as siblings to their templates. When saving to a separate directory,
+  # the directory structure of the templates is preserved.
+  # Params:
+  # +out+:: directory to save generated files to.
+
+  def render(out=@directory)
+    @queue.each do |f|
+      template = ERB.new(File.read File.join(@directory, f))
+      rendered = template.result(binding)
+      basename = File.basename f, ".erb"
+      dirname = File.dirname f
+      outfile = File.join(out, dirname, basename + ".html")
+
+      FileUtils.mkdir_p(File.dirname(outfile)) if (not File.exist? File.dirname(outfile))
+      File.write(outfile, rendered)
+
+      p "Transformed #{ f } -> #{ outfile }"
+    end
+  end
+
+  ##
+  # Utility function that can be called from inside .erb files to render mixins.
+  # You can optionally pass in an +env+ hash with variables you want accessible from
+  # inside the mixin - these get added to the binding object, so the mixin can still
+  # call methods from this class if necessary.
+  # Params:
+  # +file+:: mixin or erb file to render
+  # +env+:: optional hash of variables we want accessible to the template.
+  # Returns:
+  # string
+
+  def render_internal(file, env={})
+    template = ERB.new(File.read File.join(@directory, file))
+    b = binding
+    env.each do |key, val|
+      # This allows us to access values from inside nested templates
+      b.local_variable_set(key.to_sym, val)
+    end
+    template.result b
+  end
+
+end
---
+++ spec/renderer_spec.rb
@@ -0,0 +1,84 @@
+require_relative '../commander/renderer.rb'
+require 'fileutils'
+
+describe Renderer do
+
+  before :all do
+    FileUtils.mkdir_p("render_tests/nested")
+    index_content = "<h1>Hello</h1><%= 1 + 2 %>"
+    nested_content = "<p>nested <%= 5 + 6 %></p>"
+    File.write("render_tests/index.erb", index_content)
+    File.write("render_tests/nested/_nested.erb", nested_content)
+  end
+
+  describe "[]" do
+    it "initializes a renderer with a queue" do
+       renderer = Renderer['render_tests'] 
+       expect(renderer.queue.empty?).to be false
+    end
+  end
+
+  describe "render" do
+    it "renders template files as siblings" do
+      Renderer['render_tests'].render
+      expect(File.exist? "render_tests/index.html").to be true
+    end
+
+    it "does not render _template files" do
+      Renderer['render_tests'].render
+      expect(File.exist? "render_tests/nested/_index.html").to be false
+    end
+
+    it "correctly renders self-contained content" do
+      Renderer['render_tests'].render
+      expected = "<h1>Hello</h1>3"
+      expect(File.read "render_tests/index.html").to eq(expected)
+    end
+
+    it "correctly renders self-contained content to new dir" do
+      Renderer['render_tests'].render("./render_dist")
+
+      expect(File.exist? "./render_dist/index.html").to be true
+      expect(File.exist? "./render_tests/index.html").to be false
+      FileUtils.rm_rf("./render_dist")
+    end
+
+    it "correctly preserves folder structure in new dir" do
+      File.write("render_tests/nested/another.erb", "<p>Hi</p>")
+      Renderer['render_tests'].render("./render_dist")
+
+      expect(File.exist? "./render_dist/index.html").to be true
+      expect(File.exist? "./render_tests/index.html").to be false
+      expect(File.exist? "./render_dist/nested/another.html").to be true
+      expect(File.exist? "./render_tests/nested/another.html").to be false
+      FileUtils.rm_rf("./render_dist")
+    end
+  end
+
+  describe "render_internal" do
+    it "correctly renders references" do
+      index_content = "<%= render_internal \"nested/_nested.erb\" %>"
+      File.write('render_tests/index.erb', index_content)
+      result = Renderer['render_tests'].render_internal('index.erb')
+      expect(result).to eq("<p>nested 11</p>")
+    end
+
+    it "correctly passes data to references" do
+      index_content = "<%= render_internal \"nested/_nested.erb\", {a: 'data'} %>"
+      File.write('render_tests/index.erb', index_content)
+      nested_content = "<p>nested <%= a %></p>"
+      File.write('render_tests/nested/_nested.erb', nested_content)
+      result = Renderer['render_tests'].render_internal('index.erb')
+      expect(result).to eq("<p>nested data</p>")
+    end
+  end
+
+  after :each do
+    FileUtils.rm(Dir['render_tests/**/*.html'])
+  end
+
+  after :all do
+    FileUtils.rm_rf("render_tests")
+  end
+
+end
