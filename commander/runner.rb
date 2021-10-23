e5e338a7c8b2be6c063dfaf5bd83a31d4a770bc1
Nikolai Kozak <nikokozak@gmail.com>
2021-10-22 18:37:37 -0600

    listener only flag

--- commander/runner.rb
+++ commander/runner.rb
@@ -3,11 +3,9 @@ require_relative 'site.rb'
 
 site = Site.new
 
-listener = Listen.to(*ARGV) do |modified, added, removed|
-  if modified.any? { |m| /(sass|scss|erb|svg)\z/.match(m) }
-    site.render
-    p "updating site...."
-  end
+listener = Listen.to(*ARGV, only: /(sass|scss|erb|svg)\z/) do |modified, added, removed|
+  site.render
+  p "updating site...."
 end
 
 listener.start
