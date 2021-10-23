87d5190f7bd58e103cfeaf6d832b67d963530227
Nikolai Kozak <nikokozak@gmail.com>
2021-10-22 23:28:37 -0600

    add documentation

--- commander/sass.rb
+++ commander/sass.rb
@@ -1,33 +1,59 @@
 require 'pathname'
 
-# RUNS FROM THE SHELL CWD
+##
+# Functionality for dealing with Sass (scss, sass) files.
 
 class Sass
 
-  def self.filename(file)
-    /\A(.*\/)?([a-zA-Z0-9\-_]*)\.s(a|c)ss\z/.match(file)[2]
+  ##
+  # Checks if a +file+ is a css file.
+  # Params:
+  # +file+:: a filename or filepath string.
+
+  def self.is_css?(file)
+    /\.css\z/.match?(file)
   end
+
+  ##
+  # Checks if a +file+ is a sass file. A sass file is defined as a file ending
+  # in either sass or scss.
+  # Params:
+  # +file+:: a filename or filepath string.
 
   def self.is_sass?(file)
-    /\.s(a|c)ss\z/.match(file) ? true : false
+    /\.s(a|c)ss\z/.match?(file)
   end
+
+  ##
+  # Checks if a +file+ is a sass mixin. Mixins are defined as files that start
+  # with a "_" and end in either .sass or .scss.
+  # Params:
+  # +file+:: a filename or filepath string.
 
   def self.is_mixin?(file)
-    /\A(.*\/)?_.*\.s(a|c)ss\z/.match(file) ? true : false
+    /\A(.*\/)?_.*\.s(a|c)ss\z/.match?(file)
   end
 
+  ##
+  # Call the SASS transpiler on the +input_file+. Outputs a css file if successful.
+  # Params:
+  # +input_file+:: a filepath string, evaluated relative to the cwd.
+ 
   def self.call_sass(input_file)
-    input_path = Pathname.new(input_file)
-
-    fh = input_path.dirname
-    fo = File.join(fh, Sass.filename(input_file) + '.css')
-    %x{sass #{input_file} #{fo}}
-    p "Sass compiled #{ input_file } -> #{ fo }"
+    output_file = input_file.sub(/\.[a-zA-Z0-9]+\z/, ".css")
+    %x{sass #{input_file} #{output_file}}
+    p "Sass compiled #{ input_file } -> #{ output_file }"
   end
 
-  def self.run(file_path='.')
-    Dir['**/*', base: file_path].each do |f|
-      f = File.join(file_path, f)
+  ##
+  # Call the SASS transpiler on a given +directory+. Will search all subdirs
+  # recursively, evaluating sass files, skipping mixins and others.
+  # Params:
+  # +directory+:: A directory path.
+
+  def self.run(directory='.')
+    Dir['**/*', base: directory].each do |f|
+      f = File.realpath(f, directory)
 
       File.file?(f) && is_sass?(f) && (not is_mixin? f) ? call_sass(f) : next
     end
--- commander/tags.rb
+++ commander/tags.rb
@@ -1,22 +1,40 @@
 require_relative 'sass.rb'
 
+##
+# Handles the creation of "tag trees", i.e. hashes with nodes describing
+# our svg tags and their styles.
+
 class Tags
-  def initialize(folder="tags")
-    @folder = folder
+
+  ##
+  # Creates a Tag class initialized to a base +directory+, where "tags" (i.e. folders
+  # representing categories, within which pairings of svg files and sass or css files
+  # of the same name) should be found.
+  # Params:
+  # +directory+:: A directory string
+
+  def initialize(directory="tags")
+    @directory = directory
   end
 
-  def process
-    p "Processing tags in folder '#{ @folder }'"
-    Sass.run(@folder)
-    p "Finished processing tags in folder '#{ @folder }'"
-  end
+# def build
+#   p "Processing tags in folder '#{ @directory }'"
+#   Sass.run(@directory)
+#   p "Finished processing tags in folder '#{ @directory }'"
+# end
+
+  ##
+  # Creates a tree-like structure of nodes carrying tag information.
+  # Looks for an svg file, from which it derives its matching css file.
+  # The parent folder for any given svg file is used as its category,
+  # which becomes the key in the map holding it and its sibling tags.
 
   def tree
-    Dir.chdir(@folder) do
+    Dir.chdir(@directory) do
       Dir['**/*'].reduce({}) do |result, f|
-        if File.file?(f) && Sass.is_sass?(f)
+        if File.file?(f) && is_svg?(f)
           node = build_node(f)
-          cat = category_from_string(node[:category])
+          cat = node[:category]
           result[cat] = (result[cat] || []).push(node)
           result
         else
@@ -28,55 +46,100 @@ class Tags
 
   private
 
+  ##
+  # Builds a node for the tag tree using a +file+ (svg) as reference.
+  # In order to build successfully, there must be an associated css
+  # file with the same basename as the svg file passed into the function.
+  # Params:
+  # +file+:: Filename as string (svg file)
+
   def build_node(file)
-    filepath = Pathname.new(file)
-    category = parent_folder(filepath) == "." ? @folder : parent_folder(filepath)
-    filename = filename(file)
-    title = filename_to_title(filename)
-    css_path = check_css(File.join(filepath.dirname, filename + ".css"))
+    parent = parent_folder(file)
+    category = parent == "." ? to_symbol(@directory) : to_symbol(parent)
+    basename = basename(file)
+    title = basename_to_title(basename)
+    css_path = ensure_exists(change_extension(file, ".css"), "css")
     style = File.read(css_path)
-    svg_path = check_svg(File.join(filepath.dirname, filename + ".svg"))
+    svg_path = file
     svg_contents = File.read(svg_path)
 
     { title: title,
-      filename: filename,
+      basename: basename,
       style: style,
-      svg_file: svg_path,
+      svg_path: svg_path,
       svg_contents: svg_contents,
-      path: filepath.to_s,
+      path: File.realpath(file),
       category: category }
   end
 
-  def filename(file)
-    /([a-zA-Z0-9\-_]+)\..+\z/.match(file)[1]
+  ##
+  # Extracts the basename from a filename.
+  # Params:
+  # +file+:: filename string
+
+  def basename(file)
+    File.basename(file, ".*")
   end
 
-  def parent_folder(pathname)
-    pathname.dirname.basename.to_s
+  ##
+  # Returns the parent folder for the file. Uses Pathname internally.
+  # Params:
+  # +file+:: a filename string
+
+  def parent_folder(file)
+    Pathname.new(file).dirname.basename.to_s
   end
 
-  def filename_to_title(filename)
-    filename.gsub(/[\-_]/, " ").capitalize
+  ##
+  # Removes dashes and underscores from a +basename+, replacing them with
+  # spaces.
+  # Params:
+  # +basename+:: a basename string
+
+  def basename_to_title(basename)
+    basename.gsub(/[\-_]/, " ").capitalize
   end
 
-  def category_from_string(cs)
-    cs.gsub(/[\-_]/, '').to_sym
+  ##
+  # Santitizes a +string+ by removing dashes and underscores, converting it
+  # into a symbol.
+  # Params:
+  # +string+:: a string
+
+  def to_symbol(string)
+    string.gsub(/[\-_]/, '').to_sym
   end
 
-  def check_css(css_path)
-    if not File.exist?(css_path)
-      throw Error.new("No css style found for file #{ file }. Please process and try again.")
+  ##
+  # Ensure a given +file+ exists. Uses File.file? internally.
+  # Params:
+  # +file+:: filepath string to check.
+
+  def ensure_exists(file)
+    if not File.file?(file)
+      throw Error.new("No #{ File.extname(file) } file found at #{ file }. Please ensure it exists, or that you have transpiled the necessary files.")
     else
-      css_path
+      path
     end
   end
 
-  def check_svg(svg_path)
-    if not File.exist?(svg_path)
-      throw Error.new("No svg file found for #{ file }. Please include one and try again.")
-    else
-      svg_path
-    end
+  ##
+  # Replace a +file+'s extension with a +new_extension+. New extension must include the period.
+  # Params:
+  # +file+:: filename string
+  # +new_extension+:: extension string - must include period
+
+  def change_extension(file, new_extension)
+    file.sub(/\.[a-zA-Z0-9]+\z/, new_extension)
+  end
+
+  ##
+  # Check if a given +file+ is an svg.
+  # Params:
+  # +file+:: filename string
+
+  def is_svg?(file)
+    /\.svg\z/.match?(file)
   end
 
 end
